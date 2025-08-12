f258e3cf36e33fdb34618b645d7f13cfe0eade50
ronaldpostma <admin@ronaldpostma.com>
2025-08-12 12:21:37 +0200

    basic index.php update

--- brro-flex-theme/index.php
+++ brro-flex-theme/index.php
@@ -1,12 +1,17 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The main template file - Fallback only
+ *
+ * @package Brro_Flex_Theme
+ */
 
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-    <?php endwhile; ?>
-<?php else : ?>
-    <p><?php _e('No posts found.', 'brro-flex-theme'); ?></p>
-<?php endif; ?>
+get_header(); ?>
+
+<div>
+    <div>
+        <h1><?php bloginfo('name'); ?></h1>
+        <p><?php _e('Dit is het fallback-sjabloon. Als je dit ziet, maak dan een specifieke sjabloon voor dit paginatype, of neem contact op met de ontwikkelaar via', 'brro-flex-theme'); ?> <a href="mailto:support@brro.nl">support@brro.nl</a>.</p>
+    </div>
+</div>
 
 <?php get_footer(); ?>
